aeb00d9dab1bfc086bf7f52df832314aca80f479
Levia <cr1o0127@gmail.com>
2024-05-24 16:56:46 +0800

    update: add font setting in GlobalStyle.ts

--- src/constants/GlobalStyle.ts
+++ src/constants/GlobalStyle.ts
@@ -2,5 +2,10 @@ import { createGlobalStyle } from "styled-components";
 import { reset } from "./reset";
 
 export const GlobalStyle = createGlobalStyle`
-    ${reset}
+  ${reset}
+  h1,h2,h3,h4,h5,p,a,li{
+    font-family: sans-serif;
+    text-decoration: none;
+    line-height: 1.5;
+  }
 `;
