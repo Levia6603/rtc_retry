d6aae0c9b7f4b0956a50d075a73c1fcaf8192518
Levia <cr1o0127@gmail.com>
2024-05-24 16:03:25 +0800

    chore: add styled-components and firebase packages

---
+++ src/server/firebase.ts
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCeVWh34HE7GKBKi1FRBNaCa9_jmgLB580",
+  authDomain: "rtcretry-3d8ca.firebaseapp.com",
+  projectId: "rtcretry-3d8ca",
+  storageBucket: "rtcretry-3d8ca.appspot.com",
+  messagingSenderId: "456759284357",
+  appId: "1:456759284357:web:e02b184fb4e0588cd68fec",
+  measurementId: "G-HF63W4RN1R",
+};
