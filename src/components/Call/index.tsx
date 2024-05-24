c24c2a95893324a20b47cad1bdd1aa910d05bbe5
Levia <cr1o0127@gmail.com>
2024-05-24 20:13:16 +0800

    style: complete login page

--- src/components/Call/index.tsx
+++ src/components/Call/index.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import { Container, Wrapper } from "./style";
 
 function Call() {
+  const [id, setId] = useState("");
   return (
     <Wrapper>
-      <Container>123</Container>
+      <Container>
+        <h2>Enter Your Room Key</h2>
+        <label>
+          <input
+            type="text"
+            value={id}
+            onChange={(e) => {
+              setId(e.target.value);
+            }}
+          />
+          <button type="button" onClick={() => {}}>
+            Enter
+          </button>
+        </label>
+        <p>or</p>
+        <button type="button">create room</button>
+      </Container>
     </Wrapper>
   );
 }
--- src/components/Call/style.ts
+++ src/components/Call/style.ts
@@ -10,8 +10,43 @@ export const Wrapper = styled.section`
 `;
 
 export const Container = styled.div`
-  max-width: 1200px;
-  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  width: 1000px;
+  height: 600px;
   border: 2px solid #000;
   border-radius: 20px;
+  background-color: #fff;
+  box-shadow: 10px 10px 0px #000;
+  h2 {
+    font-size: 36px;
+    font-weight: 800;
+  }
+  input {
+    font-size: 16px;
+    margin-right: 8px;
+    padding: 8px 16px;
+    border: 1px solid #000;
+    border-radius: 4px;
+    background-color: #fff;
+  }
+  button {
+    padding: 8px 16px;
+    border: 1px solid #000;
+    border-radius: 8px;
+    color: #fff;
+    background-color: #000;
+    font-size: 16px;
+    &:hover {
+      color: #000;
+      background-color: transparent;
+    }
+    &:active {
+      color: #fff;
+      background-color: #000;
+    }
+  }
 `;
